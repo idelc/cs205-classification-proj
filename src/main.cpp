c5a32c302a2c265ce131f71e0e70093dd7bf4958
idelc <idel012@ucr.edu>
2023-06-04 16:23:25 -0700

    File Reader: Space separated
    The synthetic data files given can be read by the functions in reader.h as long as all the values contain decimals. CSV file reading not yet implemented for final stage of testing

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,33 @@
 #include "search_algorithms.h"
 
 int main(){
+    // test bench
+    unsigned row = 0, col = 0;
+    string small_file = "data/CS170_small_Data__29.txt";
+    string large_file = "data/CS170_large_Data__2.txt";
+    string huge_file = "data/CS170_XXXlarge_Data__21.txt";
+
+    get_file_dims_ssf(small_file, row, col);
+    cout << "For file " << small_file << " got: " << row << " rows and " << col << " columns" << endl;
+    double* arr1;
+    arr1 = (double *) malloc(sizeof(double)*row*col);
+    read_ssf(small_file,arr1);
+    cout << arr1[0] << ' ' << arr1[1] << ' ' << arr1[(row*col)-2] << ' ' << arr1[(row*col)-1] << endl;
+    free(arr1);
+
+    get_file_dims_ssf(large_file, row, col);
+    cout << "For file " << large_file << " got: " << row << " rows and " << col << " columns" << endl;
+    arr1 = (double *) malloc(sizeof(double)*row*col);
+    read_ssf(large_file,arr1);
+    cout << arr1[0] << ' ' << arr1[1] << ' ' << arr1[(row*col)-2] << ' ' << arr1[(row*col)-1] << endl;
+    free(arr1);
+
+    get_file_dims_ssf(huge_file, row, col);
+    cout << "For file " << huge_file << " got: " << row << " rows and " << col << " columns" << endl;
+    arr1 = (double *) malloc(sizeof(double)*row*col);
+    read_ssf(huge_file,arr1);
+    cout << arr1[0] << ' ' << arr1[1] << ' ' << arr1[(row*col)-2] << ' ' << arr1[(row*col)-1] << endl;
+    free(arr1);
+
     return 0;
 }
