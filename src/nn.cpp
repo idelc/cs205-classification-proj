1f6a08e4cf3e73ba1be12c35713188d1297cef0c
Ratnodeep Bandyopadhyay <rband003@ucr.edu>
2023-06-13 14:58:21 -0700

    nn works

--- src/nn.cpp
+++ src/nn.cpp
@@ -1,12 +1,14 @@
 #include "nn.h"
 #include "reader.h"
 #include <float.h>
+#include <ostream>
 
 #define CLASS_INDEX 0
+// #define DEBUG_MODE
 
 // Initialize the file data struct
 void init_file_data(file_data *fdat, std::string filename) {
-    get_file_dims_ssf(filename, fdat->dpt_size, fdat->total_dpts);
+    get_file_dims_ssf(filename, fdat->total_dpts, fdat->dpt_size);
     fdat->data = (double *)malloc(sizeof(double) * fdat->dpt_size * fdat->total_dpts);
     read_ssf(filename, fdat->data);
 }
@@ -16,16 +18,22 @@ double nn_eval(file_data *fdat, std::vector<int> sel_feat) {
     int accuracy = 0;
     // Test each point (by leaving it out)
     for (int i = 0; i < fdat->total_dpts; i++) {
+        #ifdef DEBUG_MODE
+          cout << "Starting new point: Class: " << get_data_at(fdat, i, 0) << endl;
+        #endif
         int closest_pt = 0;
         double min_dist = DBL_MAX;
         // find the closest point to the target point at idx i
         for(int j = 1; j < fdat->total_dpts; j++) {
             // skip the target point from consideration
-            if (j == i) j++;
+            if (j == i) continue;
             // find the distance based on select features
             double dist = compute_distance(fdat, sel_feat, i, j);
             if (dist < min_dist) {
-                closest_pt = i;
+                #ifdef DEBUG_MODE
+                  cout << "distance updated to node " << j << ": " << dist << " from " << min_dist << " class: " << get_data_at(fdat, i, 0) << endl;
+                #endif
+                closest_pt = j;
                 min_dist = dist;
             }
         }
